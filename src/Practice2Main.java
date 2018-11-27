fdfe76b6812139ab2243afa805a39b6967063585
FYNJY <tigrik0507@gmail.com>
2018-11-27 18:48:08 +0200

    Initial commit

---
+++ src/Practice2Main.java
@@ -0,0 +1,11 @@
+
+public class Practice2Main {
+    public static void main (String [] args) {
+     StringBuilder sb = new StringBuilder("Anton");
+     StringBuilder sb1 = new StringBuilder("Slavik");
+     sb.delete(0, 1);
+
+
+
+    }
+}
